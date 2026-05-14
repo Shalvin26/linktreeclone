487e2905960c9e8e8953be4690ccd8eddbb670e4
Shalvin Sankhyan <shalvin26@Shalvins-MacBook-Air.local>
2026-05-14 17:10:59 +0530

    added vercel url to cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,14 @@ const analyticsRoutes = require('./routes/analytics');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'https://linktreeclone-black.vercel.app'
+  ]
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
   if (req.method === 'OPTIONS') {
